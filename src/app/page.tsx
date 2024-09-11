d6fe7851f8c5469e75d39656af042036d4a62391
CarrotEasy0214 <seongmin0214@kakao.com>
2024-09-11 09:16:29 +0900

    edit:CheckTailwindCss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import MySlide from "@/Components/Slide/Myslide";
-import Prod from "@/Components/ProductPage/Prod";
+// import MySlide from "@/Components/Slide/Myslide";
+// import Prod from "@/Components/ProductPage/Prod";
 // // import ProductPage from "@/Components/ProductPage/ProductPage";
-import Card from "@/Components/Card/Card";
-import CompanyGuide from "@/Components/Craft/CompanyGuide";
-import Collabo from "@/Components/Collabo/Collabo";
-import MandD from "@/Components/M&D/MandD";
-import ShippingDetail from "@/Components/Shipping/ShippingDetail";
-import SupportCenter from "@/Components/Support/SupportCenter";
+// import Card from "@/Components/Card/Card";
+// import CompanyGuide from "@/Components/Craft/CompanyGuide";
+// import Collabo from "@/Components/Collabo/Collabo";
+// import MandD from "@/Components/M&D/MandD";
+// import ShippingDetail from "@/Components/Shipping/ShippingDetail";
+// import SupportCenter from "@/Components/Support/SupportCenter";
 // import BlackContainer from "@/Components/BlackContainer/BlackContainer";
-import BlackCon from "@/Components/BlackContainer/BlackCon";
+// import BlackCon from "@/Components/BlackContainer/BlackCon";
 // // import TTest from "@/Components/BlackContainer/TTest";
 // import Prod from "@/Components/ProductPage/Prod";
 // import MySlideMobile from "@/Components/Slide/MySlideMobile";
@@ -18,16 +18,17 @@ import BlackCon from "@/Components/BlackContainer/BlackCon";
 export default function Home() {
   return (
     <>
-      <MySlide></MySlide>
+      <div className="bg-black">ㅎㅇ</div>
+      {/* <MySlide></MySlide>
       <Prod></Prod>
       <Card></Card>
       <CompanyGuide></CompanyGuide>
       <Collabo></Collabo>
       <MandD></MandD>
       <ShippingDetail></ShippingDetail>
-      <SupportCenter></SupportCenter>
+      <SupportCenter></SupportCenter> */}
       {/* <BlackContainer></BlackContainer> */}
-      <BlackCon></BlackCon>
+      {/* <BlackCon></BlackCon> */}
     </>
   );
 }
