e1c776593a08420372f7d82e1c39e90ac4aaf461
CarrotEasy0214 <seongmin0214@kakao.com>
2024-09-11 10:02:20 +0900

    edit:CheckTailwindCss(8)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
+        <Notice></Notice>
         <MainLogo></MainLogo>
         {children}
       </body>
